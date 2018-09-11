f787385381c787eaf95b8f62622fbe77a201e3f0
Steven Zapata <szapata427@gmail.com>
2018-09-11 17:51:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
 data.each do |seasonnum, info|
   if seasonnum == season 
     info[:name].each do |info, value|
-      
+      first = in
       
   
 end
