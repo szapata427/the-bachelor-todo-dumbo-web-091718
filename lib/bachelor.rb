0654f10d2b35288977d6361bcef9c7dfa18b571f
Steven Zapata <szapata427@gmail.com>
2018-09-11 17:57:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,17 +10,30 @@ end
   first
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
+  
+  
+  
+  
 end
+
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
 end
 
+
+
+
 def get_occupation(data, hometown)
   # code here
 end
+
+
+
 
 def get_average_age_for_season(data, season)
   # code here
